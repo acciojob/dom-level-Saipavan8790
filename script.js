89f31efc287359ae0a4ce233a024c4a600af40cd
Saipavan8790 <127079433+Saipavan8790@users.noreply.github.com>
2023-10-31 00:14:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const itemList = Array.from(document.getElementsByTagName("li"));
 for(let i=0; i < itemList.length; i++){
 	if(itemList[i].id){
 		if(itemList[i].id === "level"){
-			alert(The level of the element is: ${i+1});
+			alert('The level of the element is: ${i+1})';
 		}
 	}
 }
